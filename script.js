f589976cf3ba8d9053e9f045e29afcee944cbdc0
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-01-27 23:16:52 +0100

    add:const

--- script.js
+++ script.js
@@ -3,9 +3,36 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.*/
 
+
+const biciDaCorsa = [
+
+    { nome: "Speedster 3000", peso: 7.5 },
+
+    { nome: "Lightning Pro", peso: 6.8 },
+    { nome: "Carbon X", peso: 7.2 },
+
+    { nome: "Aero Racer", peso: 7.9 },
+
+    { nome: "Mountain Climber", peso: 6.5 }
+];
+
 /*Snack2
 Creare un array di oggetti di squadre di calcio.
 Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console*/
+
+const squadreCalcio = [
+
+    { nome: "Juventus", punti: 0, falliSubiti: 0 },
+
+    { nome: "Milan", punti: 0, falliSubiti: 0 },
+
+    { nome: "Inter", punti: 0, falliSubiti: 0 },
+
+    { nome: "Napoli", punti: 0, falliSubiti: 0 },
+
+    { nome: "Roma", punti: 0, falliSubiti: 0 }
+
+];
